462ca63f368ecd092ccde6e94ffcda8bd518e7aa
Jonson-gel <2904180191@qq.com>
2025-04-21 22:47:26 +0100

    Debug.

--- src/main/java/com/gel/web/eneloop/config/CorsConfig.java
+++ src/main/java/com/gel/web/eneloop/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 匹配所有路径
-                        .allowedOrigins("https://jonson-gel.github.io") // 允许的前端域名
+                        .allowedOrigins("*") // 允许的前端域名
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 如果用到了cookie之类的，要加这句
